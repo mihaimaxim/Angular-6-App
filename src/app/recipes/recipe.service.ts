b7fe7d608bc53678316adceca01229f45bdad0e5
Mihai Maxim <mihaimaxim90@icloud.com>
2018-09-23 09:05:35 +0300

    menu change

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,30 @@
 import { EventEmitter } from "@angular/core";
 
 import { Recipe } from "./recipe-list/recipe.model";
+import { Ingredient } from "../shared-elements/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('test recipe', 'this is simply a test', 
-        'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
-        new Recipe('another test recipe', 'this is simply a test', 
-        'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+        new Recipe(
+            'quattro formaggi',
+            'the best',
+            'http://www.meteoweb.eu/wp-content/uploads/2014/08/pizza-formaggi.jpg',
+            [
+                new Ingredient('cheese', 1),
+                new Ingredient('peperoncino', 1)
+            ]
+        ),
+        new Recipe(
+            'burger with fries',
+            'also good',
+            'https://static.olocdn.net/menu/applebees/db21d0fba2f611d95ba3d79bda75178c.jpg',
+            [
+                new Ingredient('meat', 1),
+                new Ingredient('fries', 20)
+            ]
+        ),
     ];
 
     getRecipes() {
