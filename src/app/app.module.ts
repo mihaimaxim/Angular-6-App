1609f571009f6345246ca1801416f8dec92d1339
Mihai Maxim <mihaimaxim90@icloud.com>
2018-09-27 23:49:17 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FooterComponent } from './footer/footer.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './/app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingListComponent,
     ShoppingEditComponent,
     FooterComponent,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe-list/recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -15,7 +15,8 @@ export class RecipeDetailComponent implements OnInit {
 
 	constructor(
 		private recipeService: RecipeService,
-		private route: ActivatedRoute
+		private route: ActivatedRoute,
+		private router: Router
 	) { }
 
 	ngOnInit() {
@@ -29,5 +30,9 @@ export class RecipeDetailComponent implements OnInit {
 
 	onAddIngredientsToShoppingList() {
 		this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+	}
+
+	onEdit() {
+		this.router.navigate(['edit'], {relativeTo: this.route})
 	}
 }
