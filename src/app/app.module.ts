a1f7c8ea101eb7716fb203321c789c42a7d74d02
Mihai Maxim <mihaimaxim90@icloud.com>
2018-10-11 16:51:20 +0300

    feature modules / lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
